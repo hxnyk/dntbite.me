e88fda71005c505db98948715268d4b189b9c095
Neil Parikh <parikh.neil@me.com>
2015-02-28 18:06:36 -0500

    Clean up code

--- web-server.py
+++ web-server.py
@@ -1,8 +1,17 @@
 # Main web server
 from flask import Flask, request
-app = Flask(__name__)
 from twilio.rest import TwilioRestClient
 import os
+
+app = Flask(__name__)
+
+def send_message_to(number, message):
+    account_sid = os.environ.get("TWILIO_SID")
+    auth_token = os.environ.get("TWILIO_AUTH")
+    client = TwilioRestClient(account_sid, auth_token)
+    message = client.messages.create(body=message,
+        to=number,
+        from_="2262402483")
 
 @app.route("/bite", methods=['POST'])
 def bite():
@@ -14,16 +23,6 @@ def bite():
     else:
         return "end"
         # add time to db for end
-
-def send_message_to(number, message):
-    account_sid = os.environ.get("TWILIO_SID")
-    auth_token = os.environ.get("TWILIO_AUTH")
-    client = TwilioRestClient(account_sid, auth_token)
-    message = client.messages.create(body=message,
-        to=number,
-        from_="2262402483")
-    print message.sid
-
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)

da57c874be4e939769ef76ed1c2663b8aa662700
Neil Parikh <parikh.neil@me.com>
2015-03-01 03:15:01 -0500

    debug

--- web-server.py
+++ web-server.py
@@ -35,10 +35,10 @@ def bite():
     else:
         counts[date] = 1
 
-    a = requests.put("https://dntbite.firebaseio.com/users/neil/counts.json", data =json.dumps(counts))
+    a = requests.put("https://dntbite.firebaseio.com/users/neil/counts.json", data=json.dumps(counts))
     requests.put("https://dntbite.firebaseio.com/users/neil/lastBittenTime.json", data=to_send)
     requests.post("https://dntbite.firebaseio.com/users/neil/biteTimes.json", data=to_send).text
-    return a.text
+    return json.dumps(counts)
 
 @app.route("/<file_name>.<ext>")
 def send_file(file_name, ext):
